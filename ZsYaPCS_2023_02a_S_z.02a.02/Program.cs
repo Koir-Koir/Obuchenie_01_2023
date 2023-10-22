7e2eb811c0a320bcc7e819ec31b4911f0426d1ca
Koir-Koir <DanilovDenisrune@ya.ru>
2023-10-22 22:46:11 +0500

    ZsYaPCS_2023_02a_S

---
+++ ZsYaPCS_2023_02a_S_z.02a.02/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задание 2а.С.2. Задача 13: Напишите программу, которая выводит 
+// третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число, из которого хотите извлечь третью с начала цифру -> ");
+int chislo = Convert.ToInt32(Console.ReadLine());
+int cyfra3;
+if (chislo < 0) {chislo = -chislo;}
+if (chislo < 100) {Console.Write("У введённого числа нет третьей цифры...");}
+    else {while (chislo > 999) {chislo = chislo/10;}
+    cyfra3 = chislo%10;
+    Console.Write(cyfra3);}
