0f09b4bc905c3211645fb7655d03598a9aa1d3f2
Koir-Koir <DanilovDenisrune@ya.ru>
2023-10-24 23:51:03 +0500

    ZnSYaPr_Cs_S.03a_z.01_1_Vozvedenie_A_v_stepen_B

---
+++ ZsYaPCS_2023_03a_S_z.03a.01/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задание 03a.S.01. Задача 25: Напишите цикл, который принимает на вход
+// два числа (A и B) и возводит число A в натуральную степень B.
+
+int ReadInt (string message)                //Создаём функцию, которая принимает сообщение на вывод и возвращает число;
+    {Console.Write(message);                //Функция начинает выполнять действия: показывает переданное сообщение
+    string value = Console.ReadLine();      //Мы объявляем временную переменную, чтобы принять ввод пользователя из консоли
+    return int.Parse(value);                //Функция возвращает число, которое получает с помощью преобразования значения строки в тип инт
+}
+
+int StepenChisla (int A, int B){
+    int result = 1;
+    for (int i = 1; i<= B; i++){
+        result *= A;
+    }
+    return result;
+}
+
+int osnovanie = ReadInt ("Для получения числа в степени, ведите : \tоснование A = ");
+int stepen    = ReadInt (                              "\t\t\t\t\t  степень B = ");
+
+if (stepen >= 0) {Console.WriteLine($"\t\t\t\t\t  результат = {StepenChisla(osnovanie, stepen)}.");}
+else {Console.WriteLine($"К сожалению, эта программа не может посчитать степень {stepen} - у неё лапки :\'(");}
