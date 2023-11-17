81cf368813cb8e1862a629b2d0ee3402bd5c4716
Koir-Koir <DanilovDenisrune@ya.ru>
2023-11-17 12:46:24 +0500

    ZnSYaPr_Cs_S.05a_z.01_Attestation_Recursiya_vyvod_naturalnyh_chisel_ot_M_do_N

---
+++ ZsYaPCS_2023_05b_S_z05b.01/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задание 05b.S.01.
+ // Рекурсия
+ // Вывод чисел 
+ // Задача 64: Задайте значения M и N. Напишите программу, которая
+ // выведет все чётные натуральные числа в промежутке от M до N
+ // с помощью рекурсии.
+//      
+
+// Ошибки до компиляции
+ // не убрал при копировании int
+ // не указал void рекурсивной функции
+ // 
+//
+
+using System;
+
+class Program{
+    static public void Main(string[] args){
+
+        int ReadInt (string message){
+            Console.Write(message);                
+            string value = Console.ReadLine();      
+            return int.Parse(value);                
+        }
+
+        bool CheckingValuesPrintDiapazon(int argumentFrom, int argumentTo){
+            if (argumentTo > 0 && argumentTo >= argumentFrom){
+                return true;
+            } else if (argumentTo < argumentFrom){
+                Console.WriteLine("Вы ввели некорректный диапазон! Введите вначале меньшее число, а затем большее.");
+                return false;
+            } else {
+                Console.WriteLine($"Между \"{argumentFrom}\" и \"{argumentTo}\" натуральных целых чисел нет!");
+                return false;
+            }
+        }
+
+        void PrintNaturalNumbers(int diapazonFrom, int diapazonTo){
+            if (diapazonFrom > diapazonTo){return;}
+            if (diapazonFrom > 0) {Console.Write($"{diapazonFrom}\t");}
+            PrintNaturalNumbers(diapazonFrom + 1, diapazonTo);
+        }
+
+        int printDiapazonFromM = ReadInt("Введите число (M) от которого Вы хотите " 
+                                       +$"увидеть все натуральные числа : ");
+        int printDiapazonToN = ReadInt("Введите число (N) до которого Вы хотите " 
+                                    +$"увидеть все натуральные числа : ");
+
+        if (CheckingValuesPrintDiapazon(printDiapazonFromM, printDiapazonToN)){
+            PrintNaturalNumbers(printDiapazonFromM, printDiapazonToN);
+        }
+    }
+}
